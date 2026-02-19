d00e32d4b8428c75fe916c1de1aa4bfd02b391df
arpitp2019 <arpitchamp1996@gmail.com>
2026-02-19 16:33:59 +0530

    noset

--- collectionsPractice/Noset.java
+++ collectionsPractice/Noset.java
@@ -1,15 +1,16 @@
 package collectionsPractice;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 public class Noset {
     public static void main(String[] args) {
         List<String> list1 = new ArrayList<>(List.of("Delhi", "Mumbai", "Chennai"));
         List<String> list2 = new ArrayList<>(List.of("Mumbai", "Kolkata", "Delhi"));
+
         System.out.println(list1);
         System.out.println(list2);
+
         for (int i = 0; i < list2.size(); i++) {
             if (list1.contains(list2.get(i))) {
                 continue;
